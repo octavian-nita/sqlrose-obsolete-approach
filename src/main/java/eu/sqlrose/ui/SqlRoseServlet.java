c8be6e2495e7ca010c2b87309db4157aeadb6547
Octavian Nita <mnothro@yahoo.com>
2016-07-06 18:49:39 +0200

    bootstrapping a Vaadin application

--- src/main/java/eu/sqlrose/ui/SqlRoseServlet.java
+++ src/main/java/eu/sqlrose/ui/SqlRoseServlet.java
@@ -1,5 +1,6 @@
 package eu.sqlrose.ui;
 
+import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.ServiceException;
 import com.vaadin.server.SessionDestroyEvent;
 import com.vaadin.server.SessionDestroyListener;
@@ -7,14 +8,31 @@ import com.vaadin.server.SessionInitEvent;
 import com.vaadin.server.SessionInitListener;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.server.VaadinServletService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
  * @version 1.0, Jul 05, 2016
  */
+@WebServlet(value = "/*", asyncSupported = true)
+@VaadinServletConfiguration(productionMode = false, ui = SqlRoseUI.class)
 public class SqlRoseServlet extends VaadinServlet implements SessionInitListener, SessionDestroyListener {
+
+    static {
+        SLF4JBridgeHandler.install();
+    }
+
+    private static final long serialVersionUID = 1020160705L;
+
+    private final Logger LOG = LoggerFactory.getLogger(SqlRoseServlet.class);
 
     @Override
     protected void servletInitialized() throws ServletException {
@@ -26,13 +44,39 @@ public class SqlRoseServlet extends VaadinServlet implements SessionInitListener
     }
 
     @Override
+    public void service(ServletRequest request, ServletResponse response) {
+        try {
+            super.service(request, response);
+        } catch (Throwable throwable) {
+            String message = "An error has occurred";
+
+            if (request instanceof HttpServletRequest) {
+                HttpServletRequest httpRequest = (HttpServletRequest) request;
+                message += " while servicing request " + httpRequest.getRequestURI();
+
+                String queryString = httpRequest.getQueryString();
+                if (queryString != null && (queryString = queryString.trim()).length() > 0) {
+                    message += "?" + queryString;
+                }
+            }
+
+            LOG.error(message, throwable);
+        }
+    }
+
+    @Override
     public void sessionInit(SessionInitEvent sessionInitEvent) throws ServiceException {
-        // TODO: 1. decide upon a configuration and load it
-        // TODO: 2. set up Vaadin and application logging
+        LOG.info("SqlRose session created");
+
+        // TODO: load server configuration...
+        // TODO: set up i18n...
+        // TODO: ensure local storage...
     }
 
     @Override
     public void sessionDestroy(SessionDestroyEvent sessionDestroyEvent) {
+        LOG.info("SqlRose session destroyed");
+
         // TODO: close / log remaining open DB connections...
     }
 }
--- src/main/java/eu/sqlrose/ui/SqlRoseUI.java
+++ src/main/java/eu/sqlrose/ui/SqlRoseUI.java
@@ -2,7 +2,11 @@ package eu.sqlrose.ui;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
@@ -11,6 +15,18 @@ import com.vaadin.ui.UI;
 @Theme("valo")
 public class SqlRoseUI extends UI {
 
+    private static final long serialVersionUID = 1020160705L;
+
+    private final Logger LOG = LoggerFactory.getLogger(SqlRoseUI.class);
+
     @Override
-    protected void init(VaadinRequest vaadinRequest) {}
+    protected void init(VaadinRequest vaadinRequest) {
+        VerticalLayout content = new VerticalLayout();
+        content.setMargin(true);
+        setContent(content);
+
+        content.addComponent(new Label("Hello SqlRose!"));
+
+        LOG.info("SqlRose UI initialized");
+    }
 }
