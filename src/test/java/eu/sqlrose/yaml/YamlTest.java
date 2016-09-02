ace1dafa68edff8c689622719f42d2040d3b2c2f
Octavian Nita <mnothro@yahoo.com>
2016-09-02 16:35:59 +0200

    from jackson to snakeyaml, ph#2: better understanding of snakeyaml

--- src/test/java/eu/sqlrose/yaml/YamlTest.java
+++ src/test/java/eu/sqlrose/yaml/YamlTest.java
@@ -7,6 +7,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author <a href="mailto:Octavian.NITA@ext.ec.europa.eu">Octavian NITA</a>
  * @version $Id$
@@ -23,12 +34,48 @@ public class YamlTest {
 
     @Test
     public void basicLoading() {
-        String docs = doc("# YAML", "- The Dagger 'Narthanc'", "- The Dagger 'Nimthanc'", "- The Dagger 'Dethanc'");
-        System.out.println(joln("Loading YAML documents from", docs, NL));
+        String val = "The Dagger 'Nimthanc'";
+        String src = doc("# YAML", "- The Dagger 'Narthanc'", "-   " + val + " ", "- The Dagger 'Dethanc'");
 
-        int count = 1;
-        for (Object doc : yaml.loadAll(docs)) {
-            System.out.println("DOC #" + count++ + ": " + doc.getClass());
+        Object doc = yaml.loadAll(src);
+        assertTrue("Yaml#loadAll() expected to return a List for source " + src, doc instanceof List);
+
+        List lov = (List) doc;
+        assertEquals(3, lov.size());
+
+        assertEquals(val, lov.get(1));
+    }
+
+    @Test
+    public void advancedLoading() throws IOException {
+
+        try (Reader src = new FileReader("yaml-test.yaml")) {
+            Map<?, ?> empty = null;
+            Map<?, ?> proxy = null;
+
+            for (Object doc : yaml.loadAll(src)) {
+                if (doc instanceof Map && ((Map) doc).get("empty") != null) {
+                    empty = (Map) ((Map) doc).get("empty");
+                }
+                if (doc instanceof Map && ((Map) doc).get("proxy") != null) {
+                    proxy = (Map) ((Map) doc).get("proxy");
+                }
+            }
+
+            if (empty != null) {
+                assertEquals("value1", empty.get("name1"));
+                assertEquals("", empty.get("name2"));
+                assertNull(empty.get("name3"));
+                assertNull(empty.get("name4"));
+            } else {
+                throw new RuntimeException("no test value ('proxy') defined in test fixture");
+            }
+
+            if (proxy != null) {
+                assertSame(proxy.get("http"), proxy.get("https"));
+            } else {
+                throw new RuntimeException("no test value ('proxy') defined in test fixture");
+            }
         }
     }
 
