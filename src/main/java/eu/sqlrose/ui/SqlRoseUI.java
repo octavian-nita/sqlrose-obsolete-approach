4885007acac42d89f88be4dffb0dc97850ed5fdb
Octavian Nita <mnothro@yahoo.com>
2016-10-26 18:09:50 +0200

    more interesting casing: SQLrose

--- src/main/java/eu/sqlrose/ui/SqlRoseUI.java
+++ src/main/java/eu/sqlrose/ui/SqlRoseUI.java
@@ -3,14 +3,12 @@ package eu.sqlrose.ui;
 import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
-import com.vaadin.server.Page;
 import com.vaadin.server.Responsive;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 import eu.sqlrose.core.DataSource;
 import eu.sqlrose.env.Environment;
@@ -23,7 +21,7 @@ import org.slf4j.LoggerFactory;
  * @version 1.0, Jul 05, 2016
  */
 @Theme("sqlrose")
-@Title("SQLRose")
+@Title("SQLrose")
 @PreserveOnRefresh
 public class SqlRoseUI extends UI {
 
