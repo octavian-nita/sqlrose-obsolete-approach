a12344120ce000e275d7d27500a223ce29643dfa
Octavian Nita <octavian.nita@gmail.com>
2017-06-21 17:58:27 +0200

    enter the fuzzy Catalog

--- src/main/java/eu/sqlrose/catalog/Catalog.java
+++ src/main/java/eu/sqlrose/catalog/Catalog.java
@@ -1,17 +1,66 @@
 package eu.sqlrose.catalog;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+
 /**
+ * @param <LM> the type of Lookup Matches returned by the {@link #lookup(CharSequence)} method
  * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
  * @version 1.0, Jun 20, 2017
  */
-public interface Catalog {
+public abstract class Catalog<LM> {
 
-    interface Entry {
+    protected final Collection<CharSequence> terms;
 
-        String value();
+    protected Catalog(Collection<? extends CharSequence> terms) {
+        this.terms = terms == null ? new ArrayList<>() : new ArrayList<>(terms);
     }
 
-    List<Entry> search(String pattern);
+    protected Catalog(CharSequence... terms) { this(terms == null ? null : asList(terms)); }
+
+    protected abstract LM match(CharSequence pattern, CharSequence term);
+
+    public List<LM> lookup(CharSequence pattern) {
+        if (pattern == null) {
+            return emptyList();
+        }
+
+        final List<LM> matches = new ArrayList<>();
+        for (CharSequence term : terms) {
+            if (term != null) {
+                LM lookupMatch = match(pattern, term);
+                if (lookupMatch != null) {
+                    matches.add(lookupMatch);
+                }
+            }
+        }
+        return matches;
+    }
+
+    public void add(CharSequence term) {
+        if (term != null) {
+            this.terms.add(term);
+        }
+    }
+
+    public void addAll(Collection<? extends CharSequence> terms) {
+        if (terms != null) {
+            for (CharSequence term : terms) {
+                if (term != null) {
+                    this.terms.add(term);
+                }
+            }
+        }
+    }
+
+    public void setTerms(Collection<? extends CharSequence> terms) {
+        clear();
+        addAll(terms);
+    }
+
+    public void clear() { this.terms.clear(); }
 }
---
+++ src/main/java/eu/sqlrose/catalog/FuzzyMatch.java
@@ -0,0 +1,18 @@
+package eu.sqlrose.catalog;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
+ * @version 1.0, Jun 21, 2017
+ */
+public class FuzzyMatch {
+
+    private final CharSequence term;
+
+    public FuzzyMatch(CharSequence term) {
+        this.term = requireNonNull(term);
+    }
+
+    public CharSequence getTerm() { return term; }
+}
