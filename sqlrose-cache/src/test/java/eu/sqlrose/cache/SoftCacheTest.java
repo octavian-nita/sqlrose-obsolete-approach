48068f5c46a4f1fcde0ff9e1bee5615677f603d1
octavian <octavian.nita@gmail.com>
2017-10-18 01:44:26 +0200

    testing a bit the poor man's cache

--- sqlrose-cache/src/test/java/eu/sqlrose/cache/SoftCacheTest.java
+++ sqlrose-cache/src/test/java/eu/sqlrose/cache/SoftCacheTest.java
@@ -1,12 +1,66 @@
 package eu.sqlrose.cache;
 
+import org.junit.*;
+
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
+
 /**
+ * TODO http://www.vogella.com/tutorials/Mockito/article.html
+ * TODO https://zeroturnaround.com/rebellabs/how-to-mock-up-your-unit-test-environment-to-create-alternate-realities/
+ *
  * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
  * @version 1.0, Oct 17, 2017
  */
 public class SoftCacheTest {
 
-    public void testCachingWorks() {
-        final SoftCache<String, String> cache = new SoftCache<>(2);
+    protected SoftCache<String, String> cacheUnderTest;
+
+    @Before
+    public void setUp() { cacheUnderTest = new SoftCache<>(2); }
+
+    @After
+    public void tearDown() { cacheUnderTest = null; }
+
+    @Test
+    public void testCacheInitialState() {
+        assertEquals("Cache should store 0 initial entries (size)", 0, cacheUnderTest.size());
+        assertEquals("Cache should have 2 available entries (max size)", 2, cacheUnderTest.maxSize());
+    }
+
+    @Test
+    public void testCacheStateAfterInitialMiss() {
+
+        final CountingSuffix computation = new CountingSuffix(" => value #1");
+
+        final String firstKey = "key #1";
+        final String firstVal = cacheUnderTest.getOrCompute(firstKey, computation);
+        final String firstRec = computation.apply(firstKey);
+
+        assertEquals("Cache should store 1 entry (size)", 1, cacheUnderTest.size());
+        assertEquals("Cache should store computed value", firstRec, firstVal);
+        assertEquals("Cache should not invoke computation twice", 2, computation.getCallCount());
+    }
+
+    private static final class CountingSuffix implements Function<String, String> {
+
+        private int callCount;
+
+        private final Object suffix;
+
+        CountingSuffix(Object suffix) { this.suffix = suffix; }
+
+        int getCallCount() { return callCount; }
+
+        @Override
+        public String apply(String stem) {
+            try {
+                System.out.println("Applying suffix '" + suffix + "' to stem '" + stem + "'...");
+                return stem + suffix;
+            } finally {
+                callCount++;
+            }
+        }
     }
 }
