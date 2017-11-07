85137d101414a14a3a215f1213728a414725ff11
Octavian Nita <octavian.nita@gmail.com>
2017-11-07 17:43:27 +0100

    final touch on that cache... (?)

--- fuss4j-cache/src/main/java/eu/fuss4j/cache/SoftCache.java
+++ fuss4j-cache/src/main/java/eu/fuss4j/cache/SoftCache.java
@@ -10,16 +10,16 @@ import java.util.function.Function;
 /**
  * A poor man's <strong>bounded</strong> cache with a <em>minimal interface</em> that:
  * <ul>
- * <li>is thread-safe (concurrent)</li>
- * <li>uses a {@link SoftReference}-wrapped {@link Map} instance as the backing structure to hold actual data;
- * whether or not {@code null} keys are allowed depends on the {@link Map} instance which can be customized by
- * overriding {@link #createBoundedCache(int)}</li>
+ * <li>is <strong>thread-safe</strong> (concurrent)</li>
+ * <li>uses a {@link SoftReference}-wrapped {@link Map} instance as the backing structure to hold cached data;
+ * whether or not {@code null} keys are allowed depends on this instance which can be customized by overriding
+ * {@link #createBoundedCache(int)}</li>
  * </ul>
  * By default, a custom {@link LinkedHashMap} instance is used which allows {@code null} keys and implements a
  * basic <a href="https://en.wikipedia.org/wiki/Cache_replacement_policies#LRU">LRU algorithm</a>.
  *
  * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
- * @version 1.1, Oct 17, 2017
+ * @version 2.0, Nov 7, 2017
  * @see <a href="https://www.ibm.com/developerworks/library/j-jtp01246/index.html">Plugging memory leaks with soft
  * references</a>
  * @see <a href="https://dzone.com/articles/weak-soft-and-phantom-references-in-java-and-why-they-matter">Weak, Soft,
@@ -29,13 +29,13 @@ public class SoftCache<K, V> {
 
     protected static final int DEFAULT_MAX_SIZE = 1024;
 
-    protected transient SoftReference<Map<K, V>> cacheRef;
-
     protected final int maxSize;
 
     protected final ReadWriteLock rwLock;
 
-    /** Equivalent to calling '{@code new SoftCache({@link #DEFAULT_MAX_SIZE})}'. */
+    protected transient SoftReference<Map<K, V>> cacheRef;
+
+    /** Equivalent to calling '<code>new SoftCache({@link #DEFAULT_MAX_SIZE})</code>'. */
     public SoftCache() { this(DEFAULT_MAX_SIZE); }
 
     public SoftCache(int maxSize) {
@@ -47,10 +47,14 @@ public class SoftCache<K, V> {
         this.cacheRef = new SoftReference<>(createBoundedCache(this.maxSize));
     }
 
-    public V getOrCompute(K key, Function<? super K, ? extends V> mappingFn) {
+    public V getOrCompute(K key, Function<? super K, ? extends V> computation) {
         rwLock.writeLock().lock();
         try {
-            return cache().computeIfAbsent(key, mappingFn);
+            Map<K, V> cache = cacheRef == null ? null : cacheRef.get();
+            if (cache == null) {
+                cacheRef = new SoftReference<>(cache = createBoundedCache(maxSize));
+            }
+            return cache.computeIfAbsent(key, computation);
         } finally {
             rwLock.writeLock().unlock();
         }
@@ -59,9 +63,30 @@ public class SoftCache<K, V> {
     public V get(K key) {
         rwLock.readLock().lock();
         try {
-            return cache().get(key);
+            Map<K, V> cache = cacheRef == null ? null : cacheRef.get();
+            return cache == null ? null : cache.get(key);
         } finally {
             rwLock.readLock().unlock();
+        }
+    }
+
+    public boolean contains(K key) {
+        rwLock.readLock().lock();
+        try {
+            Map<K, V> cache = cacheRef == null ? null : cacheRef.get();
+            return cache != null && cache.containsKey(key);
+        } finally {
+            rwLock.readLock().unlock();
+        }
+    }
+
+    public V remove(K key) {
+        rwLock.writeLock().lock();
+        try {
+            Map<K, V> cache = cacheRef == null ? null : cacheRef.get();
+            return cache == null ? null : cache.remove(key);
+        } finally {
+            rwLock.writeLock().unlock();
         }
     }
 
@@ -75,31 +100,30 @@ public class SoftCache<K, V> {
     }
 
     public int size() {
-        Map<K, V> cache = cacheRef == null ? null : cacheRef.get();
-        return cache == null ? 0 : cache.size();
+        rwLock.readLock().lock();
+        try {
+            Map<K, V> cache = cacheRef == null ? null : cacheRef.get();
+            return cache == null ? 0 : cache.size();
+        } finally {
+            rwLock.readLock().unlock();
+        }
     }
 
     public int maxSize() { return maxSize; }
 
-    public boolean isFull() { return size() == maxSize(); }
-
-    public boolean contains(K key) { return cache().containsKey(key); }
-
-    /**
-     * Provides convenient access to the backing {@link Map structure} that holds actual data. It is not required that
-     * this method be synchronized (it is not by default).
-     */
-    protected Map<K, V> cache() {
-        Map<K, V> cache = cacheRef == null ? null : cacheRef.get();
-        if (cache == null) {
-            cacheRef = new SoftReference<>(cache = createBoundedCache(maxSize));
+    public boolean isFull() {
+        rwLock.readLock().lock();
+        try {
+            return size() == maxSize();
+        } finally {
+            rwLock.readLock().unlock();
         }
-        return cache;
     }
 
     /**
-     * Override in order to change the implementation details of the actual storage structure. It is not required that
-     * this method be synchronized (it is not by default).
+     * Override in order to change the implementation details of the actual storage structure.
+     * <p>
+     * It is not required that this method be synchronized (it is not by default).
      *
      * @param maxSize the maximum number of elements the cache is able to store
      * @return a new instance of the backing {@link Map structure} to hold data
